a6505af248573d3617beacfe84ca4c87b95b71a0
Alejandro Cruz Paz <alejandrop@backbase.com>
2022-09-11 03:33:40 +0200

    Add download from PDF endpoint

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,12 @@
 class LandingController < ApplicationController
   def index
   end
+  
+  def download_pdf
+    respond_to do |format|
+      format.pdf do
+        send_file "#{Rails.root}/app/assets/docs/Alejandro Cruz - Mobile Software Engineer - Resume.pdf", type: "application/pdf", :disposition => "attachment"
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   get 'subscribers/create'
+  get 'download_cv', to: 'landing#download_pdf'
+  
   root to: "landing#index"
   
   resources :subscribers
